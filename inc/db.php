b0b24bef0069896217a977db871c40294b4aaaa1
Webforce3 <denis.nerborac@wf3.fr>
2015-07-03 14:20:53 +0200

    Add admin database connexion + getDb function

---
+++ inc/db.php
@@ -0,0 +1,24 @@
+<?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'admin');
+
+try {
+
+    global $db;
+    $db_options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+    );
+
+    $db = getDb(DB);
+
+} catch (Exception $e) {
+    exit('MySQL Connect Error >> '.$e->getMessage());
+}
+
+function getDb($db) {
+	return new PDO('mysql:host='.HOST.';dbname='.$db.'', USER, PASS, $db_options);
+}
