427d6284b623b652f28847e58fff8c5e49abb471
alyrodriguez <aly.rodrgz@gmail.com>
2015-07-03 17:26:29 +0200

    Update sidebar-left - Add mailbox menu + compose mail + FIX header config + FIX global db_options

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,9 @@
-<?php require_once $root_dir.'/inc/db.php' ?>
+<?php
+
+require_once 'config.php';
+require_once $root_dir.'/inc/db.php';
+
+?>
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
