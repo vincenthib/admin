e00677b9f7a500b1ee16c5cf841e550c4625dfea
manjoker <manbanhduc@gmail.com>
2015-07-03 17:24:16 +0200

    Update read-mail man

---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'inc/db.php'
+
+$root_path = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+
+?>
--- modules/mailbox/read-mail.php
+++ modules/mailbox/read-mail.php
@@ -1,5 +1,20 @@
 <?php require_once 'config.php' ?>
-<?php include_once $root_dir.'/partials/header.php' ?>
+<?php include_once $root_dir.'/partials/header.php'; 
+
+
+if (empty($_GET['id'])) {
+  exit('Paramètre id manquant');
+}
+
+$id = intval($_GET['id']);
+
+$query = $db->prepare('SELECT * FROM mailbox WHERE id = :id');
+$query->bindValue('id', $id, PDO::PARAM_INT);
+$query->execute();
+$mail = $query->fetch();
+
+if (!empty($mail)) {
+?>
 
         <!-- Content Header (Page header) -->
         <section class="content-header">
@@ -60,9 +75,11 @@
                   </div>
                 </div><!-- /.box-header -->
                 <div class="box-body no-padding">
-                  <div class="mailbox-read-info">
-                    <h3>Message Subject Is Placed Here</h3>
-                    <h5>From: support@almsaeedstudio.com <span class="mailbox-read-time pull-right">15 Feb. 2015 11:03 PM</span></h5>
+      <!-- jjjjjjjjjj -->           
+                <div class="mailbox-read-info">
+                  <!-- jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj -->
+                    <h3>Objet: <?= $mail['objet'] ?></h3>
+                    <h5><!-- From: support@almsaeedstudio.com --> From: <?= $mail['expediteur'] ?><span class="mailbox-read-time pull-right"><!-- 15 Feb. 2015 11:03 PM --><?= $mail['received'] ?></span></h5>
                   </div><!-- /.mailbox-read-info -->
                   <div class="mailbox-controls with-border text-center">
                     <div class="btn-group">
@@ -73,12 +90,13 @@
                     <button class="btn btn-default btn-sm" data-toggle="tooltip" title="Print"><i class="fa fa-print"></i></button>
                   </div><!-- /.mailbox-controls -->
                   <div class="mailbox-read-message">
-                    <p>Hello John,</p>
+                    <?= $mail['message'] ?>
+                    <!-- <p>Hello John,</p>
                     <p>Keffiyeh blog actually fashion axe vegan, irony biodiesel. Cold-pressed hoodie chillwave put a bird on it aesthetic, bitters brunch meggings vegan iPhone. Dreamcatcher vegan scenester mlkshk. Ethical master cleanse Bushwick, occupy Thundercats banjo cliche ennui farm-to-table mlkshk fanny pack gluten-free. Marfa butcher vegan quinoa, bicycle rights disrupt tofu scenester chillwave 3 wolf moon asymmetrical taxidermy pour-over. Quinoa tote bag fashion axe, Godard disrupt migas church-key tofu blog locavore. Thundercats cronut polaroid Neutra tousled, meh food truck selfies narwhal American Apparel.</p>
                     <p>Raw denim McSweeney's bicycle rights, iPhone trust fund quinoa Neutra VHS kale chips vegan PBR&B literally Thundercats +1. Forage tilde four dollar toast, banjo health goth paleo butcher. Four dollar toast Brooklyn pour-over American Apparel sustainable, lumbersexual listicle gluten-free health goth umami hoodie. Synth Echo Park bicycle rights DIY farm-to-table, retro kogi sriracha dreamcatcher PBR&B flannel hashtag irony Wes Anderson. Lumbersexual Williamsburg Helvetica next level. Cold-pressed slow-carb pop-up normcore Thundercats Portland, cardigan literally meditation lumbersexual crucifix. Wayfarers raw denim paleo Bushwick, keytar Helvetica scenester keffiyeh 8-bit irony mumblecore whatever viral Truffaut.</p>
                     <p>Post-ironic shabby chic VHS, Marfa keytar flannel lomo try-hard keffiyeh cray. Actually fap fanny pack yr artisan trust fund. High Life dreamcatcher church-key gentrify. Tumblr stumptown four dollar toast vinyl, cold-pressed try-hard blog authentic keffiyeh Helvetica lo-fi tilde Intelligentsia. Lomo locavore salvia bespoke, twee fixie paleo cliche brunch Schlitz blog McSweeney's messenger bag swag slow-carb. Odd Future photo booth pork belly, you probably haven't heard of them actually tofu ennui keffiyeh lo-fi Truffaut health goth. Narwhal sustainable retro disrupt.</p>
                     <p>Skateboard artisan letterpress before they sold out High Life messenger bag. Bitters chambray leggings listicle, drinking vinegar chillwave synth. Fanny pack hoodie American Apparel twee. American Apparel PBR listicle, salvia aesthetic occupy sustainable Neutra kogi. Organic synth Tumblr viral plaid, shabby chic single-origin coffee Etsy 3 wolf moon slow-carb Schlitz roof party tousled squid vinyl. Readymade next level literally trust fund. Distillery master cleanse migas, Vice sriracha flannel chambray chia cronut.</p>
-                    <p>Thanks,<br>Jane</p>
+                    <p>Thanks,<br>Jane</p> -->
                   </div><!-- /.mailbox-read-message -->
                 </div><!-- /.box-body -->
                 <div class="box-footer">
@@ -86,7 +104,7 @@
                     <li>
                       <span class="mailbox-attachment-icon"><i class="fa fa-file-pdf-o"></i></span>
                       <div class="mailbox-attachment-info">
-                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-paperclip"></i> Sep2014-report.pdf</a>
+                        <a href="#" class="mailbox-attachment-name"><i class="fa fa-paperclip"></i> Sep2014-report.pdf </a>
                         <span class="mailbox-attachment-size">
                           1,245 KB
                           <a href="#" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
@@ -134,9 +152,12 @@
                   <button class="btn btn-default"><i class="fa fa-print"></i> Print</button>
                 </div><!-- /.box-footer -->
               </div><!-- /. box -->
+
+              
             </div><!-- /.col -->
           </div><!-- /.row -->
         </section><!-- /.content -->
+        <?php } ?>
       </div><!-- /.content-wrapper -->
       <footer class="main-footer">
         <div class="pull-right hidden-xs">
