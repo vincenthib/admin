0c2ab1b61c0e730335df9f988b833f8dc8626655
iskandir59 <manalegende2@gmail.com>
2015-07-06 15:40:55 +0200

    FIX conflict modules stats footer

--- modules/stats/partials/footer-charts.php
+++ modules/stats/partials/footer-charts.php
@@ -32,13 +32,13 @@ $(function () {
 	* Here we will create a few charts using ChartJS
 	*/
 
-	$.ajax({  
-	url: 'modules/stats/browser-usage-data.php',  
-	async: true,  
-	type: 'POST',  
-	dataType: 'json',  
-	success: function(data) {  
-	
+	$.ajax({
+	url: 'modules/stats/browser-usage-data.php',
+	async: true,
+	type: 'POST',
+	dataType: 'json',
+	success: function(data) {
+
 		//-------------
 		//- PIE CHART -
 		//-------------
@@ -82,14 +82,14 @@ $(function () {
 			}
 		}
 
-	},  
-	error: function(data){  
+	},
+	error: function(data){
 		console.log(data);
-		console.log('Erreur');  
-	}  
+		console.log('Erreur');
+	}
 	});
 
-	
+
 
   //-------------
   //- PIE CHART -
@@ -166,105 +166,6 @@ $(function () {
     pieChart.Doughnut(PieDatas, pieOption);
   }
 
-<<<<<<< HEAD
-	var data = <?= json_encode($users_stats_result) ?>
-
-	//-------------
-	//- BAR CHART -
-	//-------------
-	if ($("#barChart").length > 0) {
-
-		var barChartCanvas = $("#barChart").get(0).getContext("2d");
-		var barChart = new Chart(barChartCanvas);
-		var barChartData = data;
-		barChartData.datasets[0].fillColor = "#00a65a";
-		barChartData.datasets[0].strokeColor = "#00a65a";
-		barChartData.datasets[0].pointColor = "#00a65a";
-		var barChartOptions = {
-			//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-			scaleBeginAtZero: true,
-			//Boolean - Whether grid lines are shown across the chart
-			scaleShowGridLines: true,
-			//String - Colour of the grid lines
-			scaleGridLineColor: "rgba(0,0,0,.05)",
-			//Number - Width of the grid lines
-			scaleGridLineWidth: 1,
-			//Boolean - Whether to show horizontal lines (except X axis)
-			scaleShowHorizontalLines: true,
-			//Boolean - Whether to show vertical lines (except Y axis)
-			scaleShowVerticalLines: true,
-			//Boolean - If there is a stroke on each bar
-			barShowStroke: true,
-			//Number - Pixel width of the bar stroke
-			barStrokeWidth: 2,
-			//Number - Spacing between each of the X value sets
-			barValueSpacing: 5,
-			//Number - Spacing between data sets within X values
-			barDatasetSpacing: 1,
-			//String - A legend template
-			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-			//Boolean - whether to make the chart responsive
-			responsive: true,
-			maintainAspectRatio: false
-		};
-
-		barChartOptions.datasetFill = false;
-		barChart.Bar(barChartData, barChartOptions);
-	}
-
-	//--------------
-	//- AREA CHART -
-	//--------------
-	if ($("#areaChart").length > 0) {
-
-		// Get context with jQuery - using jQuery's .get() method.
-		var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
-		// This will get the first returned node in the jQuery collection.
-		var areaChart = new Chart(areaChartCanvas);
-
-		var areaChartOptions = {
-			//Boolean - If we should show the scale at all
-			showScale: true,
-			//Boolean - Whether grid lines are shown across the chart
-			scaleShowGridLines: false,
-			//String - Colour of the grid lines
-			scaleGridLineColor: "rgba(0,0,0,.05)",
-			//Number - Width of the grid lines
-			scaleGridLineWidth: 1,
-			//Boolean - Whether to show horizontal lines (except X axis)
-			scaleShowHorizontalLines: true,
-			//Boolean - Whether to show vertical lines (except Y axis)
-			scaleShowVerticalLines: true,
-			//Boolean - Whether the line is curved between points
-			bezierCurve: true,
-			//Number - Tension of the bezier curve between points
-			bezierCurveTension: 0.3,
-			//Boolean - Whether to show a dot for each point
-			pointDot: false,
-			//Number - Radius of each point dot in pixels
-			pointDotRadius: 4,
-			//Number - Pixel width of point dot stroke
-			pointDotStrokeWidth: 1,
-			//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-			pointHitDetectionRadius: 20,
-			//Boolean - Whether to show a stroke for datasets
-			datasetStroke: true,
-			//Number - Pixel width of dataset stroke
-			datasetStrokeWidth: 2,
-			//Boolean - Whether to fill the dataset with a color
-			datasetFill: true,
-			//String - A legend template
-			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-			//Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-			maintainAspectRatio: false,
-			//Boolean - whether to make the chart responsive to window resizing
-			responsive: true
-		};
-
-		//Create the line chart
-		areaChart.Line(data, areaChartOptions);
-	}
-
   var areaChartData = {
     labels: <?= json_encode($years) ?>,
     datasets: <?= json_encode($line_data) ?>
@@ -322,14 +223,12 @@ $(function () {
   lineChartOptions.datasetFill = false;
   lineChart.Line(areaChartData, lineChartOptions);
 
-=======
-
-	$.ajax({  
-		url: 'modules/stats/user-stats-data.php',  
-		async: true,  
-		type: 'POST',  
-		dataType: 'json',  
-		success: function(data) {  
+	$.ajax({
+		url: 'modules/stats/user-stats-data.php',
+		async: true,
+		type: 'POST',
+		dataType: 'json',
+		success: function(data) {
 
 			//-------------
 			//- BAR CHART -
@@ -372,7 +271,7 @@ $(function () {
 
 				barChartOptions.datasetFill = false;
 				barChart.Bar(barChartData, barChartOptions);
-     
+
             $('.overlay').hide();
 			}
 
@@ -410,7 +309,7 @@ $(function () {
 					//Number - Pixel width of point dot stroke
 					pointDotStrokeWidth: 1,
 					//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-					pointHitDetectionRadius: 20,
+					pointHitDetectionRadius: 1,
 					//Boolean - Whether to show a stroke for datasets
 					datasetStroke: true,
 					//Number - Pixel width of dataset stroke
@@ -430,12 +329,11 @@ $(function () {
 			}
 
 
-		},  
-		error: function(){  
-			console.log('Erreur');  
-		}  
-	});  
->>>>>>> d7148ad14d0b8ca54379e415bfc4949f7cdacb63
+		},
+		error: function(){
+			console.log('Erreur');
+		}
+	});
 
 });
 </script>
