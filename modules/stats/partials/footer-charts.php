c51b12679dd02f9cb93e8ddc549817734aaf242c
vincenthib <vincent.thiblet@gmail.com>
2015-07-06 14:49:20 +0200

    passage en ajax

--- modules/stats/partials/footer-charts.php
+++ modules/stats/partials/footer-charts.php
@@ -32,44 +32,66 @@ $(function () {
 	* Here we will create a few charts using ChartJS
 	*/
 
-	//-------------
-	//- PIE CHART -
-	//-------------
-	if ($("#pieChart").length > 0) {
-		// Get context with jQuery - using jQuery's .get() method.
-		var pieChartCanvas = $("#pieChart").get(0).getContext("2d");
-		var pieChart = new Chart(pieChartCanvas);
-		var PieData = <?= json_encode($piebrowsers) ?>;
-		var pieOptions = {
-			//Boolean - Whether we should show a stroke on each segment
-			segmentShowStroke: true,
-			//String - The colour of each segment stroke
-			segmentStrokeColor: "#fff",
-			//Number - The width of each segment stroke
-			segmentStrokeWidth: 2,
-			//Number - The percentage of the chart that we cut out of the middle
-			percentageInnerCutout: 50, // This is 0 for Pie charts
-			//Number - Amount of animation steps
-			animationSteps: 100,
-			//String - Animation easing effect
-			animationEasing: "easeOutBounce",
-			//Boolean - Whether we animate the rotation of the Doughnut
-			animateRotate: true,
-			//Boolean - Whether we animate scaling the Doughnut from the centre
-			animateScale: false,
-			//Boolean - whether to make the chart responsive to window resizing
-			responsive: true,
-			// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-			maintainAspectRatio: false,
-			//String - A legend template
-			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-		};
-		//Create pie or douhnut chart
-		// You can switch between pie and douhnut using the method below.
-		pieChart.Doughnut(PieData, pieOptions);
-	}
+	$.ajax({  
+	url: 'modules/stats/browser-usage-data.php',  
+	async: true,  
+	type: 'POST',  
+	dataType: 'json',  
+	success: function(data) {  
+	
+		//-------------
+		//- PIE CHART -
+		//-------------
+		if ($("#pieChart canvas").length > 0) {
+			// Get context with jQuery - using jQuery's .get() method.
+			var pieChartCanvas = $("#pieChart canvas").get(0).getContext("2d");
+			var pieChart = new Chart(pieChartCanvas);
+			var PieData = data;
+			var pieOptions = {
+				//Boolean - Whether we should show a stroke on each segment
+				segmentShowStroke: true,
+				//String - The colour of each segment stroke
+				segmentStrokeColor: "#fff",
+				//Number - The width of each segment stroke
+				segmentStrokeWidth: 2,
+				//Number - The percentage of the chart that we cut out of the middle
+				percentageInnerCutout: 50, // This is 0 for Pie charts
+				//Number - Amount of animation steps
+				animationSteps: 100,
+				//String - Animation easing effect
+				animationEasing: "easeOutBounce",
+				//Boolean - Whether we animate the rotation of the Doughnut
+				animateRotate: true,
+				//Boolean - Whether we animate scaling the Doughnut from the centre
+				animateScale: false,
+				//Boolean - whether to make the chart responsive to window resizing
+				responsive: true,
+				// Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+				maintainAspectRatio: false,
+				//String - A legend template
+				legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+			};
+			//Create pie or douhnut chart
+			// You can switch between pie and douhnut using the method below.
+			pieChart.Doughnut(PieData, pieOptions);
 
-	//-------------
+			for(var i in data) {
+				var label = data[i].label;
+				var color = data[i].color;
+				$('#pieChart').find('.chart-legend').append('<li><i class="fa fa-circle" style="color: '+color+'">'+label+'</i></li>');
+			}
+		}
+
+	},  
+	error: function(data){  
+		console.log(data);
+		console.log('Erreur');  
+	}  
+	});
+
+	
+
+  //-------------
   //- PIE CHART -
   //-------------
   if ($("#pieChart-genres").length > 0) {
@@ -144,7 +166,7 @@ $(function () {
     pieChart.Doughnut(PieDatas, pieOption);
   }
 
-	var data = <?= json_encode($users_stats_result) ?>
+	var data = <?= json_encode($users_stats_result) ?> 
 
 	//-------------
 	//- BAR CHART -
